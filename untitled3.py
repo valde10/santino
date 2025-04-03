b879df65e42d41fea2b158d2fe61af1364dd93f2
valde10 <santinovaldemarin@gmail.com>
2025-04-03 18:44:04 -0300

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,11 @@
+clasificacion = 85
+
+if clasificacion >= 90:
+    print("exelente")
+    
+elif clasificacion >= 80:
+    print("muy bueno")
+    
+elif clasificacion >= 70:
+    print("bueno")
+    
---
+++ untitled4.py
@@ -0,0 +1,12 @@
+"""
+ingresar dos valores t comparar cual es el mayor
+"""
+valores = int(input( "ingrese el valor a):"))
+
+valor = int(input("ingrese el valor b):"))
+
+if valores > valor:
+    print("valor A es mayor")
+    
+else:
+    print("valor B es mayor")
