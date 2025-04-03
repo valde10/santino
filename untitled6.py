ea9e848f9373ce7842c11844eeb2ad89af6b1e9e
valde10 <santinovaldemarin@gmail.com>
2025-04-03 19:07:38 -0300

    Add files via upload

---
+++ untitled6.py
@@ -0,0 +1,9 @@
+#while #repite un bloque mientras una condicion se verdadera
+# contador = 3 // asigno un valor a la variable 
+# contador == 5 // hacer una comparacion
+contador=0
+while True:
+    print(contador)
+    contador +=1 
+    if contador == 5:
+        break 
